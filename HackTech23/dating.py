7f493ca8d529db441f9bdfab90164109e2b1d8db
Atheneyu <112197372+Atheneyu@users.noreply.github.com>
2023-03-04 00:46:41 -0800

    Add files via upload

--- HackTech23/dating.py
+++ HackTech23/dating.py
@@ -1,7 +1,12 @@
 class Self:
-    def __init__(self, name, ):
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+    
+    def 
 
 
 class Targets:
     def __init__(self, name, problem):
-        
+        self.name = name
+        self.problem = problem
